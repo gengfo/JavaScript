2c91fd1a528f023600ad5bbdda90c29f295753b1
gengfo <gengfo@gmail.com>
2020-04-23 23:15:46 +0800

    vue.js项目开发实战第三章

---
+++ ebook_vuejs_xiangmukaifashizhan/ch_3/movie_view/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <p>this is test p</p>
+    <table border="1">
+      <tr>
+        <td>d1</td>
+        <td>d2</td>
+      </tr>
+    </table>
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ebook_vuejs_xiangmukaifashizhan/ch_3/readme.txt
@@ -0,0 +1,1 @@
+vue init webpack movie_view #初始化项目
